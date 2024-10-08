4da68f363df85f76ad29fac66a228f7412a3d85b
keilua <keilua.richard@gmail.com>
2024-10-08 14:07:00 +0200

    card image

--- src/views/card.js
+++ src/views/card.js
@@ -1,22 +1,27 @@
+import keanuImage from '../assets/keanu.jpg';
+
 export default () => (`
-<article class="group">
+<article class="flex items-center space-x-4 p-4 bg-white shadow-lg rounded-lg">
   <img
-    alt=""
-    src="../assets/keanu.jpg"
-    class="h-56 w-full rounded-xl object-cover shadow-xl transition group-hover:grayscale-[50%]"
+    alt="Keanu Richard"
+    src="${keanuImage}"
+    class="h-32 w-32 rounded-full object-cover shadow-md"
   />
 
-  <div class="p-4">
-    <a href="#">
-      <h3 class="text-lg font-medium text-gray-900">Finding the Journey to Mordor</h3>
-    </a>
-
-    <p class="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
-      pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem,
-      mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius atque
-      dignissimos. Molestias explicabo corporis voluptatem?
+  <div class="flex-1">
+    <h3 class="text-xl font-semibold text-gray-900">Keanu Reeves</h3>
+    <p class="mt-2 text-sm text-gray-600">
+      dev web
     </p>
+
+    <div class="mt-4">
+      <a 
+        href="../assets/cv_keanu.pdf" 
+        download 
+        class="inline-block px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition">
+        Télécharger CV
+      </a>
+    </div>
   </div>
 </article>
-  `);
+`);
