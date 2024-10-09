fc15f4c47e95d000778f708216132cf45ddaabce
keilua <keilua.richard@gmail.com>
2024-10-09 13:51:26 +0200

    favicone2

--- src/views/footer.js
+++ src/views/footer.js
@@ -3,28 +3,6 @@ import logo from '../assets/logo.png';
 export default () => (`
   <footer class="bg-gray-800">
     <div class="relative mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 lg:pt-24">
-      <div class="absolute end-4 top-4 sm:end-6 sm:top-6 lg:end-8 lg:top-8">
-        <a
-          class="inline-block rounded-full bg-pink-600 p-2 text-white shadow transition hover:bg-pink-500 sm:p-3 lg:p-4"
-          href="#MainContent"
-        >
-          <span class="sr-only">Back to top</span>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="size-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </a>
-      </div>
-
       <div class="lg:flex lg:items-end lg:justify-between">
         <div class="flex flex-col items-center text-pink-300 lg:items-start">
           <img src="${logo}" alt="Logo" class="h-12 mb-4" />
